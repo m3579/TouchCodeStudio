e3054333382288167063b6cff1e2d6535b94a4c4
M3579 <mihirkasmalkar@gmail.com>
2016-04-02 21:14:20 -0700

    Implemented "skeleton" of file explorer master view and text/code editor detail view

---
+++ TouchCodeStudio/TouchCodeStudio/View/FileExplorerPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TouchCodeStudio
+{
+    public partial class FileExplorerPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+
+        public FileExplorerPage()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
